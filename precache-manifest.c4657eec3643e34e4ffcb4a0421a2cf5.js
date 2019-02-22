8751dc971df15fda6262e16c5ccda36cf54a3338
aman29271 <aman29271@gmail.com>
2019-02-22 18:26:44 +0530

    Updates

---
+++ precache-manifest.c4657eec3643e34e4ffcb4a0421a2cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8ce05037bfd6015f067a",
+    "url": "/react-app/static/js/runtime~main.8ce05037.js"
+  },
+  {
+    "revision": "a6e14d413310bf34731c",
+    "url": "/react-app/static/js/main.a6e14d41.chunk.js"
+  },
+  {
+    "revision": "01cf4f392c52f4613472",
+    "url": "/react-app/static/js/2.01cf4f39.chunk.js"
+  },
+  {
+    "revision": "a6e14d413310bf34731c",
+    "url": "/react-app/static/css/main.1b85bb75.chunk.css"
+  },
+  {
+    "revision": "01cf4f392c52f4613472",
+    "url": "/react-app/static/css/2.6f3a0075.chunk.css"
+  },
+  {
+    "revision": "5bfefc692c3b6c038a1162980f1d4636",
+    "url": "/react-app/index.html"
+  }
+];
