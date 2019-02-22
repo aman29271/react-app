52af19539419158eba0d412cb0b14f20c7876536
aman29271 <aman29271@gmail.com>
2019-02-22 15:26:36 +0530

    modified file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,60 @@
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import {Container,Col,Row,Nav,Navbar,NavDropdown,Button,Form,FormControl} from 'react-bootstrap';
+class Home extends Component{
+render(){
+  return (<div>This is home page.</div>)
+}
+}
+class Link extends Component{
+  render(){
+    return (<div>This is Link page.</div>)
+  }}
+  class Action extends Component{
+    render(){
+      return (<div>This is Action page.</div>)
+    }}
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+      
+      <Container>
+      <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="/home">Home</Nav.Link>
+                <Nav.Link href="/link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="/action">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="/action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="/action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="/action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+          </Navbar.Collapse>
+          </Navbar>
+          <Row>
+          <Col sm={8}>
+          <Route path="/" component="{Home}"></Route>
+          <Route path="/link" component="{Link}"></Route>
+          <Route path="/action" component="{Action}"></Route>
+          </Col>
+          </Row>
+        </Container>
+       
+    </div>  )
+    }
 }
 
 export default App;
+
